7cad68c4a709bb536fa66458dad5df5c99d136d6
Jamie Mancini <jamiemancini@gmail.com>
2022-09-27 19:12:23 -0700

    first commit

---
+++ running-sum.py
@@ -0,0 +1,25 @@
+#1480. Running Sum of 1d Array
+
+#Given an array nums. 
+# #We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+#Return the running sum of nums.
+
+#Input: nums = [1,2,3,4]
+#Output: [1,3,6,10]
+#Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+def running_sum_fct(list):
+    running_sum=[]
+    running_sum.append(list[0])
+    for i in range (1,len(list)):
+        running_sum.append(running_sum[i-1]+list[i])
+    return running_sum
+    
+
+nums = [1,2,3,4]
+
+
+print(running_sum_fct(nums))
+
+#time complexity is O(N) because we go over every element in our input
+#space complexity is O(1)
