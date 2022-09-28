ba552b66f119d75e3fe522c1f89271d6aa78db6b
Jamie Mancini <jamiemancini@gmail.com>
2022-09-27 20:35:34 -0700

    started algorithm

---
+++ steps-to-reduce_1342.py
@@ -0,0 +1,25 @@
+#1342. Number of Steps to Reduce a Number to Zero
+
+#Given an integer num, return the number of steps to reduce it to zero.
+
+#In one step, if the current number is even, you have to divide it by 2, 
+# otherwise, you have to subtract 1 from it.
+
+#Example 2:
+
+#Input: num = 8
+#Output: 4
+
+#Explanation: 
+#Step 1) 8 is even; divide by 2 and obtain 4. 
+#Step 2) 4 is even; divide by 2 and obtain 2. 
+#Step 3) 2 is even; divide by 2 and obtain 1. 
+#Step 4) 1 is odd; subtract 1 and obtain 0.
+
+def num_steps(num):
+    steps = 0
+    if num % 2 == 0:
+        steps = num / 2
+    else: 
+        steps= (num +1) / 2
+    return steps
